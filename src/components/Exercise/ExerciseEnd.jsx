d44c4dc3c5db852bf3df3a45540da59a7655f5ca
Kyle Jensen <kjensen19@gmail.com>
2022-10-13 16:05:45 -0500

    end finished, need to decide on saga and write server side update

--- src/components/Exercise/ExerciseEnd.jsx
+++ src/components/Exercise/ExerciseEnd.jsx
@@ -1,8 +1,30 @@
+import { useHistory } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { useEffect } from 'react'
 
 function ExerciseEnd() {
+    const history = useHistory()
+    const dispatch = useDispatch()
+
+    //Not sure if I should use the same saga as PUT or
+    //if I should make an additional saga to mark complete?
+    //either way need to refetch after and hit home
+    useEffect(() => {
+        return () => {
+            dispatch({
+                type: 'COMPLETE_EXERCISE'
+            })
+        }
+    })
+
+
+    function handleClick(){
+        history.push('/')
+    }
     //Need some kind of graphic/display here + text
     return(
         <>
+        <button onClick={handleClick}>Home</button>
         <div className="resContainer">
             <div id="panelOne">Great Job!</div>
             <div id="panelTwo">Feel Better?</div>
