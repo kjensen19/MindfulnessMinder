d61ee45a98c9150ee5a3a6500305dc630e11a903
Kyle Jensen <kjensen19@gmail.com>
2022-10-16 15:56:19 -0500

    added results list to result/chart page

--- src/components/Assesment/Results.jsx
+++ src/components/Assesment/Results.jsx
@@ -4,12 +4,19 @@ import RadarChart from "./ResultChart"
 import { useSelector } from 'react-redux'
 function Results() {
     const history = useHistory()
+    const results = useSelector(store => store.assesment)
     
     return(
         <div>
-            <h1>some info about your scores goes here</h1>
-            <RadarChart />
+            <h1>Results:</h1>
+            <ul>
+                <li>Physical stress rating {results.area_one}</li>
+                <li>Emotional stress rating {results.area_two}</li>
+                <li>Mental stress rating {results.area_three}</li>
+                <li>Psychosocial stress rating {results.area_four}</li>
+            </ul>
             <Button variant="outlined" color="inherit" onClick={() => history.push('/user')}>Home</Button>
+            <RadarChart />
         </div>
     )
 }
